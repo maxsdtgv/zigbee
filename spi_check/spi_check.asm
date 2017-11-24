632e843f20166aa6deb5fc9e31f6d4369d437e39
Maksym V <maxsdtgv@gmail.com>
2017-11-24 16:54:04 +0300

    SPI module added, compiled

---
+++ spi_check/spi_check.asm
@@ -0,0 +1,8 @@
+
+_main:
+
+;spi_check.c,1 :: 		void main() {
+;spi_check.c,3 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ spi_check/spi_check.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/VB/workspace_zigbee/spi_check/spi_check.c"
+void main() {
+
+}
